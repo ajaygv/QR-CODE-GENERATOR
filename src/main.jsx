94d782bcab6dbc8c0084d138ded7b2f0da3a36ec
ajaygv <ajaygv26@gmail.com>
2024-12-06 14:16:44 +0530

    final out

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css';
-import App from './App.jsx';
-import { LearnComponent } from './Component/LearnComponent';
-import { Hr } from './Component/header';
-import {UserCard} from "./UserCard"; 
-import { QrCode } from './Component/QrCode';
-import './QR.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <UserCard /> */}
-    <QrCode/>
+   
   </StrictMode>
 );
